4c2c359164ec502fe9446797031cb254688192aa
Yerai <yespinar.moreno@atsistemas.com>
2023-01-13 11:49:34 +0100

    added crud db

--- src/routes/db.ts
+++ src/routes/db.ts
@@ -1,11 +1,5 @@
 import { FastifyInstance } from 'fastify'
-
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-class SQLStatementORM {
-  public static selectAllUserDesc (): string {
-    return 'SELECT * FROM users ORDER BY id DESC'
-  }
-}
+import { SQLStatementORM } from '../utils/sql-queries'
 
 export default async (fastify: FastifyInstance): Promise<void> => {
   // Find all
@@ -20,11 +14,84 @@ export default async (fastify: FastifyInstance): Promise<void> => {
     }
   })
 
+  // Find all actives
+  fastify.get('/actives', async (req, res) => {
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(SQLStatementORM.selectAllActiveUserDesc())
+      return rows
+    } finally {
+      // Release the client immediately after query resolves, or upon error
+      client.release()
+    }
+  })
+
+  // Find all deleted
+  fastify.get('/deleted', async (req, res) => {
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(SQLStatementORM.selectAllDeletedUserDesc())
+      return rows
+    } finally {
+      // Release the client immediately after query resolves, or upon error
+      client.release()
+    }
+  })
+
   // Find by ID
   fastify.get<{ Params: { id: string } }>('/:id', async (req, res) => {
     const client = await fastify.pg.connect()
     try {
-      const { rows } = await client.query('SELECT * FROM users WHERE id=$1', [req.params.id])
+      const { rows } = await client.query(SQLStatementORM.selectUserById(req.params.id))
+      return rows
+    } finally {
+      // Release the client immediately after query resolves, or upon error
+      client.release()
+    }
+  })
+
+  // Find by Name
+  fastify.get<{ Params: { name: string } }>('/name/:name', async (req, res) => {
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(SQLStatementORM.selectUserByName(req.params.name))
+      return rows
+    } finally {
+      // Release the client immediately after query resolves, or upon error
+      client.release()
+    }
+  })
+
+  // Add user
+  fastify.post<{ Body: { name: string } }>('/', async (req, res) => {
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(SQLStatementORM.insertUser(req.body.name))
+      return rows
+    } finally {
+      // Release the client immediately after query resolves, or upon error
+      client.release()
+    }
+  })
+
+  // Update user
+  fastify.put<{ Body: { id: string, name: string } }>('/', async (req, res) => {
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(SQLStatementORM.updateUser(req.body.id, req.body.name))
+      return rows
+    } finally {
+      // Release the client immediately after query resolves, or upon error
+      client.release()
+    }
+  })
+
+  // Delete user
+  fastify.delete<{ Params: { id: string } }>('/:id', async (req, res) => {
+    const client = await fastify.pg.connect()
+    console.log(req.params.id)
+    try {
+      const { rows } = await client.query(SQLStatementORM.deleteUser(req.params.id))
       return rows
     } finally {
       // Release the client immediately after query resolves, or upon error
---
+++ src/utils/sql-queries.ts
@@ -0,0 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class SQLStatementORM {
+  public static selectAllUserDesc (): string {
+    return 'SELECT * FROM users ORDER BY id DESC'
+  }
+
+  public static selectAllActiveUserDesc (): string {
+    return 'SELECT * FROM users WHERE deleted_at IS NULL ORDER BY id DESC'
+  }
+
+  public static selectAllDeletedUserDesc (): string {
+    return 'SELECT * FROM users WHERE deleted_at IS NOT NULL ORDER BY id DESC'
+  }
+
+  public static selectUserById (id: string): string {
+    return `SELECT * FROM users WHERE id=${id}`
+  }
+
+  public static selectUserByName (name: string): string {
+    return `SELECT * FROM users WHERE name='${name}'`
+  }
+
+  public static insertUser (name: string): string {
+    return `INSERT INTO users(name) VALUES ('${name}') RETURNING *` // tras insertar devolvemos el registro
+  }
+
+  public static updateUser (id: string, name: string): string {
+    return `UPDATE users SET name = '${name}' WHERE id=${id} RETURNING *` // tras insertar devolvemos el registro
+  }
+
+  public static deleteUser (id: string): string {
+    return `DELETE FROM users WHERE id=${id} RETURNING *`
+  }
+}
